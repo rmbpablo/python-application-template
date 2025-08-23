8447d2cc211ad124044517577218ceb69a34988e
Pablo Barcellos <pablobarcellos@gmail.com>
2025-08-23 18:23:23 -0300

    feat: add pyproject and template structure

---
+++ src/python_application_template/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+try:
+    __version__ = version('slc_template')
+except PackageNotFoundError:
+    __version__ = '0.0.0'
---
+++ src/python_application_template/main.py
@@ -0,0 +1,12 @@
+"""Microservice entrypoint."""
+
+from __future__ import annotations
+
+
+def main() -> int:
+    """Dispatcher function for the main microservice entrypoint."""
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
